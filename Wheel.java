3d138ac1fa0c47d872d7b7963507d98a1ce75ec3
nikrp <76831568+nikrp@users.noreply.github.com>
2023-03-28 17:52:57 -0700

    Working

--- Wheel.java
+++ Wheel.java
@@ -5,22 +5,26 @@ public class Wheel {
   
   double radius;
   
+  // Wheel Constructor
   Wheel(double radius) {
     this.radius = radius;
   }
   
+  // Get the Circumference
   double getCircumference() {
-    return 2 * Math.PI * this.radius;
+    return 2 * Math.PI * radius;
   }
   
+  // Get the Area
   double getArea() {
-    return this.radius * this.radius * Math.PI;
+    return radius * radius * Math.PI;
   }
   
+  // Main
   public static void main(String[] args) {
     
     // Get the User Input
-    System.out.println("Please provide a real number: ");
+    System.out.print("Please provide a real number: ");
     Scanner s = new Scanner(System.in); 
     double input = s.nextDouble();
     
